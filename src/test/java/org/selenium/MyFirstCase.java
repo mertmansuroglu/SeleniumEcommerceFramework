04808aaf4aeec84dbce928712e70834220142706
mert.mansuroglu <mert.mansuroglu@testinium.com>
2022-04-15 11:54:31 +0300

    test commit

---
+++ src/test/java/org/selenium/MyFirstCase.java
@@ -0,0 +1,12 @@
+package org.selenium;
+
+import org.testng.annotations.Test;
+
+public class MyFirstCase {
+
+    //alwyas best practice to put the class in packages
+    @Test
+    public void dummyTest(){
+
+    }
+}
